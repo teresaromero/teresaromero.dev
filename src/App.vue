53972051ea1d5d516f2e86a028e388f1243eb87a
Teresa <tromerolozano@gmail.com>
2020-03-19 21:05:35 +0100

    fix styles

--- src/App.vue
+++ src/App.vue
@@ -11,108 +11,113 @@
 
 <template>
   <div id="app">
-    <section class="blue">
-      <div class="hero left">
-        <h1>
-          <mark>{{ $t('hero.title') }}</mark>
-        </h1>
+    <div class="container">
+      <section class="blue">
+        <div class="hero left">
+          <h1>
+            <mark>{{ $t('hero.title') }}</mark>
+          </h1>
 
-        <h2>
-          <mark>{{ $t('hero.subtitle') }}</mark>
-        </h2>
-      </div>
-    </section>
-    <section>
-      <div class="value">
-        <blockquote>¿Necesitas una página web para tu negocio?</blockquote>
-        <blockquote>¿Tienes en mente un negocio digital y no sabes como empezar?</blockquote>
-      </div>
-
-      <div class="about">
-        <div class="image">
-          <img class="avatar" src="./assets/teresa_romero.jpg" alt="teresa romero" />
+          <h2>
+            <mark>{{ $t('hero.subtitle') }}</mark>
+          </h2>
         </div>
-        <div class="description">
-          <p>Soy Teresa Romero, desarrolladora web y puedo ayudarte desde el planteamiento del diseño del producto digitial hasta su puesta en produción.</p>
-          <p>Desarrollo todo tipo de proyectos digitales para particulares y pequeños negocios.</p>
+      </section>
+      <section>
+        <div class="value">
+          <blockquote>¿Necesitas una página web para tu negocio?</blockquote>
+          <blockquote>¿Tienes en mente un negocio digital y no sabes como empezar?</blockquote>
         </div>
-      </div>
-    </section>
 
-    <section>
-      <div class="tech">
-        <h2>¿Qué tecnologias utilizo?</h2>
-        <div class="icons">
-          <div class="icon">
-            <icon-base icon-name="mdiNodejs" iconColor="#f26389" />
+        <div class="about">
+          <div class="image">
+            <img class="avatar" src="./assets/teresa_romero.jpg" alt="teresa romero" />
           </div>
-          <div class="icon">
-            <icon-base icon-name="mdiReact" iconColor="#f26389" />
-          </div>
-          <div class="icon">
-            <icon-base icon-name="mdiVuejs" iconColor="#f26389" />
-          </div>
-          <div class="icon">
-            <icon-base icon-name="mdiWordpress" iconColor="#f26389" />
-          </div>
-          <div class="icon">
-            <icon-base icon-name="mdiAws" iconColor="#f26389" />
-          </div>
-          <div class="icon">
-            <icon-base icon-name="mdiGoogleCloud" iconColor="#f26389" />
-          </div>
-          <div class="icon">
-            <icon-base icon-name="mdiGithub" iconColor="#f26389" />
-          </div>
-          <div class="icon">
-            <icon-base icon-name="mdiSass" iconColor="#f26389" />
+          <div class="description">
+            <p>Soy Teresa Romero, desarrolladora web y puedo ayudarte desde el planteamiento del diseño del producto digitial hasta su puesta en produción.</p>
+            <p>Desarrollo todo tipo de proyectos digitales para particulares y pequeños negocios.</p>
           </div>
         </div>
-      </div>
+      </section>
 
-      <div class="portfolio">
-        <h2>Ya he desarrollado estos sitios</h2>
+      <section>
+        <div class="tech">
+          <h2 class="center">¿Qué tecnologias utilizo?</h2>
+          <div class="icons">
+            <div class="icon">
+              <icon-base icon-name="mdiNodejs" iconColor="#f26389" />
+            </div>
+            <div class="icon">
+              <icon-base icon-name="mdiReact" iconColor="#f26389" />
+            </div>
+            <div class="icon">
+              <icon-base icon-name="mdiVuejs" iconColor="#f26389" />
+            </div>
+            <div class="icon">
+              <icon-base icon-name="mdiWordpress" iconColor="#f26389" />
+            </div>
+            <div class="icon">
+              <icon-base icon-name="mdiAws" iconColor="#f26389" />
+            </div>
+            <div class="icon">
+              <icon-base icon-name="mdiGoogleCloud" iconColor="#f26389" />
+            </div>
+            <div class="icon">
+              <icon-base icon-name="mdiGithub" iconColor="#f26389" />
+            </div>
+            <div class="icon">
+              <icon-base icon-name="mdiSass" iconColor="#f26389" />
+            </div>
+          </div>
+        </div>
+
         <div class="portfolio">
+          <h2 class="center">Ya he desarrollado estos sitios</h2>
           <div class="portfolio">
-            <h3 class="center">Asociacion Victoria</h3>
-            <img
-              class="portfolio-img"
-              src="./assets/asociacionvictoria.jpg"
-              alt="asociacion victoria"
-            />
-          </div>
-          <div class="portfolio">
-            <h3 class="center">Salón neo técnico de peluquería y estética Rufas</h3>
+            <div class="portfolio">
+              <h3 class="center">Asociacion Victoria</h3>
+              <a
+                class="img-anchor"
+                href="http://asociacionvictoria.es"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src="./assets/asociacionvictoria.jpg" alt="asociacion victoria" />
+              </a>
+            </div>
+            <div class="portfolio">
+              <h3 class="center">Centro de estética Rufas</h3>
 
-            <img
-              class="portfolio-img"
-              src="./assets/rufas.jpg"
-              alt="centro de estetica y peluquería rufas"
-            />
-          </div>
-          <div class="portfolio">
-            <h3 class="center">Dr. Romero Cardiel</h3>
-            <img class="portfolio-img" src="./assets/romerocardiel.jpg" alt="dr. romero" />
+              <img
+                class="portfolio-img"
+                src="./assets/rufas.jpg"
+                alt="centro de estetica y peluquería rufas"
+              />
+            </div>
+            <div class="portfolio">
+              <h3 class="center">Dr. Romero Cardiel</h3>
+              <img class="portfolio-img" src="./assets/romerocardiel.jpg" alt="dr. romero" />
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
 
-    <section class="pink">
-      <div class="action">
-        <button>Quiero informarme</button>
-      </div>
-    </section>
-    <footer>
-      <div>
-        <div class="copyright">
-          <span>© 2020 - Teresa Romero</span>
+      <section class="pink">
+        <div class="action">
+          <button>Quiero informarme</button>
         </div>
-        <div class="legal">
-          <a>Aviso Legal</a>
-          <a>Politica de Privacidad</a>
+      </section>
+      <footer>
+        <div>
+          <div class="copyright">
+            <span>© 2020 - Teresa Romero</span>
+          </div>
+          <div class="legal">
+            <a>Aviso Legal</a>
+            <a>Politica de Privacidad</a>
+          </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </div>
   </div>
 </template>

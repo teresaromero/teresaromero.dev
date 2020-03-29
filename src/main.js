170bc570e0890567736bbcfee0c118420db742c5
Teresa <tromerolozano@gmail.com>
2020-03-29 10:42:59 +0200

    refactor route

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import Vue from "vue/dist/vue.runtime.esm.js";
-import VueRouter from "vue-router/dist/vue-router.esm.js";
 import VueMeta from "vue-meta/dist/vue-meta.esm.js";
 import { Plugin } from "vue-fragment";
 import VueGtag from "vue-gtag";
 import { VueReCaptcha } from "vue-recaptcha-v3";
 import Spinner from "vue-simple-spinner/dist/vue-simple-spinner.min.js";
-
+import router from "./router";
 import App from "./App";
 
 import CookieLaw from "vue-cookie-law/dist/vue-cookie-law.js";
@@ -18,86 +17,9 @@ Vue.component("icon-base", Icon);
 Vue.component("spinner", Spinner);
 
 Vue.config.productionTip = false;
-Vue.use(VueRouter);
 Vue.use(Plugin);
 Vue.use(VueMeta);
 Vue.use(VueReCaptcha, { siteKey: process.env.VUE_APP_SITEKEY });
-
-const router = new VueRouter({
-  mode: "history",
-  routes: [
-    {
-      path: "/",
-      component: () => import("./pages/Home"),
-    },
-    {
-      path: "/aviso-legal",
-      component: () => import("./pages/AvisoLegal"),
-    },
-    {
-      path: "/politica-de-privacidad",
-      component: () => import("./pages/PoliticaPrivacidad"),
-    },
-    {
-      path: "/politica-de-cookies",
-      component: () => import("./pages/PoliticaCookies"),
-    },
-    { path: "*", component: ()=> import('./pages/NotFound') }
-  ],
-  scrollBehavior() {
-    return { x: 0, y: 0 };
-  }
-});
-
-// This callback runs before every route change, including on page load.
-router.beforeEach((to, from, next) => {
-  // This goes through the matched routes from last to first, finding the closest route with a title.
-  // eg. if we have /some/deep/nested/route and /some, /deep, and /nested have titles, nested's will be chosen.
-  const nearestWithTitle = to.matched
-    .slice()
-    .reverse()
-    .find(r => r.meta && r.meta.title);
-
-  // Find the nearest route element with meta tags.
-  const nearestWithMeta = to.matched
-    .slice()
-    .reverse()
-    .find(r => r.meta && r.meta.metaTags);
-  // const previousNearestWithMeta = from.matched
-  //   .slice()
-  //   .reverse()
-  //   .find(r => r.meta && r.meta.metaTags);
-
-  // If a route with a title was found, set the document (page) title to that value.
-  if (nearestWithTitle) document.title = nearestWithTitle.meta.title;
-
-  // Remove any stale meta tags from the document using the key attribute we set below.
-  Array.from(
-    document.querySelectorAll("[data-vue-router-controlled]")
-  ).map(el => el.parentNode.removeChild(el));
-
-  // Skip rendering meta tags if there are none.
-  if (!nearestWithMeta) return next();
-
-  // Turn the meta tag definitions into actual elements in the head.
-  nearestWithMeta.meta.metaTags
-    .map(tagDef => {
-      const tag = document.createElement("meta");
-
-      Object.keys(tagDef).forEach(key => {
-        tag.setAttribute(key, tagDef[key]);
-      });
-
-      // We use this to track which meta tags we create, so we don't interfere with other ones.
-      tag.setAttribute("data-vue-router-controlled", "");
-
-      return tag;
-    })
-    // Add the meta tags to the document head.
-    .forEach(tag => document.head.appendChild(tag));
-
-  next();
-});
 
 Vue.use(
   VueGtag,
